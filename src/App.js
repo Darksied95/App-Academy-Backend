915cde7b018a59f11c8184bbf7e932ca1792cc26
Darksied95 <aliyuefe9501@gmail.com>
2023-04-25 19:07:37 +0100

    implemented cors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const todoRouter = require("./Routes/todo.route")
 const userRouter = require("./Routes/user.route")
+const errorHandler = require('./Middleware/ErrorHandler')
 
 
 app.use(express.json())
+app.use(cors())
 app.use('/api/todos', todoRouter)
 app.use('/api/user', userRouter)
+app.use(errorHandler)
 
 
 module.exports = app
