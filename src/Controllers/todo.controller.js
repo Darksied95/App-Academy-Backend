e17f0a0c09adb132c388c1c5b4ae071b457a7a25
Darksied95 <aliyuefe9501@gmail.com>
2023-04-25 22:45:47 +0100

    wrap handlers in trycatch

--- src/Controllers/todo.controller.js
+++ src/Controllers/todo.controller.js
@@ -1,54 +1,74 @@
 const Todomodel = require('../Models/todo.model')
 
-async function getTodos(req, res) {
-    const todos = await Todomodel.findAll({
-        where: {
-            author_id: req.user.id
-        }
-    })
+async function getTodos(req, res, next) {
+    try {
+        const todos = await Todomodel.findAll({
+            where: {
+                author_id: req.user.id
+            }
+        })
 
-    res.json(todos)
+        res.json(todos)
+    } catch (error) {
+        next(error)
+    }
+
 }
 
-async function deleteTodo(req, res) {
-    console.log(req.query, req.params, 90);
-    const user = await Todomodel.destroy({
-        where: {
-            id: req.params.id,
-            author_id: req.user.id
-        }
-    })
+async function deleteTodo(req, res, next) {
+    try {
+        const user = await Todomodel.destroy({
+            where: {
+                id: req.params.id,
+                author_id: req.user.id
+            }
+        })
 
-    res.json({ type: "success", count: user })
+        res.json({ type: "success", count: user })
+    } catch (error) {
+        next(error)
+    }
+
 }
-async function updateTodo(req, res) {
-    const { completed, text } = req.body
+async function updateTodo(req, res, next) {
 
-    const updateObj = {}
+    try {
+        const { completed, text } = req.body
 
-    updateObj.completed = completed || false
+        const updateObj = {}
 
-    updateObj.text = text
+        updateObj.completed = completed || false
 
-    const user = await Todomodel.update(updateObj, {
-        where: {
-            id: req.params.id,
-            author_id: req.user.id
-        }
-    })
-    res.json({ type: "success", user })
+        updateObj.text = text
+
+        const user = await Todomodel.update(updateObj, {
+            where: {
+                id: req.params.id,
+                author_id: req.user.id
+            }
+        })
+        res.json({ type: "success", user })
+    } catch (error) {
+        next(error)
+    }
+
 }
 async function createTodo(req, res) {
+    try {
+        const { text } = req.body
 
-    const { text } = req.body
+        if (!text) throw new Error('Text cannot be empty')
 
-    if (!text) throw new Error('Text cannot be empty')
+        const todo = await Todomodel.create({
+            author_id: req.user.id,
+            text
+        })
+        res.json({ type: "success", todo })
+    } catch (error) {
+        next(error)
+    }
 
-    const todo = await Todomodel.create({
-        author_id: req.user.id,
-        text
-    })
-    res.json({ type: "success", todo })
+
 }
 
 module.exports = { getTodos, deleteTodo, updateTodo, createTodo }
--- src/Middleware/auth.js
+++ src/Middleware/auth.js
@@ -3,19 +3,24 @@ const jwt = require('jsonwebtoken')
 
 
 async function auth(req, res, next) {
-    const token = req.header('Authorization').replace('Bearer ', '')
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')
 
-    const decoded = jwt.decode(token, process.env.JWT_SIGNATURE)
+        const decoded = jwt.decode(token, process.env.JWT_SIGNATURE)
 
-    const user = await UserModel.findOne({ where: { id: decoded.id } })
+        const user = await UserModel.findOne({ where: { id: decoded.id } })
 
-    if (!user) throw new Error('User not Authenticated')
+        if (!user) throw new Error('User not Authenticated')
 
-    req.user = user.dataValues
+        req.user = user.dataValues
 
-    req.token = token
+        req.token = token
 
-    next()
+        next()
+    } catch (error) {
+        next(error)
+    }
+
 }
 
 module.exports = auth
